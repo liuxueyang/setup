a593369d5d56f3b8345357b3a690a551f6d70d0c
liuxueyang <liuxueyang457@gmail.com>
2020-12-22 00:17:40 +0800

    add xmonad wallpaper

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,13 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Wallpaper
 
-main = xmonad desktopConfig
-  {
-    terminal = "alacritty"
+main = do
+  -- randomly pick a wallpaper.
+  setRandomWallpaper ["$HOME/Pictures/wallpaper"]
+  xmonad desktopConfig
+    {
+      terminal = "alacritty"
     , modMask = mod4Mask
-  }
+    }
 
